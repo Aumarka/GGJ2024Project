ffcc853dd6927f6a49d28941496e3373cccc650a
GamerGoblins <64504813+GamerGoblins@users.noreply.github.com>
2024-01-26 21:58:28 +1000

    cracky baby eyes

---
+++ Assets/Scripts/Chair.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chair : MonoBehaviour
+{
+    public Transform SnapPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Baby")) return;
+
+        other.transform.position = SnapPoint.position;
+    }
+}
