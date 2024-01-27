3fa9de6e1fc906904605a60209d6728f9df164bd
GamerGoblins <64504813+GamerGoblins@users.noreply.github.com>
2024-01-27 21:39:36 +1000

    timer chair

--- Assets/Scripts/Chair.cs
+++ Assets/Scripts/Chair.cs
@@ -4,10 +4,44 @@ public class Chair : MonoBehaviour
 {
     public Transform BabySnapPoint, ItemSnapPoint;
 
-    private void OnCollisionEnter(Collision other)
+    public bool IsTimedChair = false;
+    public bool IsOccupied = false;
+
+    public float OccupiedTimer = 0, MaxOccupiedTimer = 5;
+
+    GameDirector _gameDirector;
+
+    public int TaskID;
+
+    void Start()
+    {
+        _gameDirector = GameObject.FindGameObjectWithTag("GameDirector").GetComponent<GameDirector>();
+    }
+
+    void Update()
+    {
+        ChairTimer();
+    }
+
+    void ChairTimer()
+    {
+        if (!IsOccupied) return;
+        if (!IsTimedChair) return;
+
+        OccupiedTimer += Time.deltaTime;
+
+        if (OccupiedTimer < MaxOccupiedTimer) return;
+        _gameDirector.CompleteTask(TaskID);
+
+        OccupiedTimer = 0;
+    }
+
+    void OnCollisionEnter(Collision other)
     {
         if (!other.gameObject.CompareTag("Baby")) return;
 
         other.transform.GetComponent<Baby>().SitDown(this, BabySnapPoint);
+
+        IsOccupied = true;
     }
 }
