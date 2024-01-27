a1aa51a3912361e6c49fafc06b0c877a27200555
Mark Auman <markryanauman@gmail.com>
2024-01-27 21:31:25 +1000

    Implemented Prototypes for Instrument, Blowing Bubbles, Rattle and Books items

---
+++ Assets/BabyInteractable.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyInteractable : Item
+{
+    GameDirector gameDirector;
+
+    public Transform Camera;
+    public float InteractRange;
+
+    public AudioClip interactionSound;
+    public GameObject interactionEffect;
+
+    public int linkedTaskID;
+
+    private void Start()
+    {
+        if (Camera == null)
+        {
+            Camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        }
+        gameDirector = gameDirector = GameObject.FindGameObjectWithTag("GameDirector").GetComponent<GameDirector>();
+    }
+
+    public override void Interact()
+    {
+        if (Physics.Raycast(Camera.position, Camera.forward, out RaycastHit hit, InteractRange))
+        {
+            if (hit.collider.gameObject.CompareTag("Baby"))
+            {
+
+                Debug.Log($"{itemName} interacted with");
+
+                if (interactionSound != null)
+                {
+                    // Include logic to play sound
+                }
+
+                if(interactionEffect != null)
+                {
+                    Instantiate(interactionEffect, transform.position, transform.rotation);
+                }
+
+                gameDirector.CompleteTask(linkedTaskID);
+            }
+        }
+    }
+}
