cb487f2bbf906ec0c004dd9926298317b40c829b
GamerGoblins <64504813+GamerGoblins@users.noreply.github.com>
2024-01-28 11:53:53 +1000

    prevent baby flying away when sitting

--- Assets/Scripts/Baby.cs
+++ Assets/Scripts/Baby.cs
@@ -27,7 +27,15 @@ public class Baby : Item
     // Update is called once per frame
     void Update()
     {
-        
+        FixedSitSpot();
+    }
+
+    void FixedSitSpot()
+    {
+        if (!IsSitting) return;
+        if (!CurrentChair) return;
+
+        SitDown(CurrentChair, CurrentChair.BabySnapPoint);
     }
 
     public override void Yeet(Vector3 force, Transform launchPoint)
